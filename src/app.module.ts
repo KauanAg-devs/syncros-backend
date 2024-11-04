284d449ce5befb7f0deca3ee10ee9fb25a42ea4c
kauanAg-devs <kauanbarcelos77@gmail.com>
2024-11-04 09:07:44 -0300

    feat: typeorm+postgres connection by docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'minha_senha',
+      database: 'nome_do_banco',
+      entities: [],
+    }),
+
+  ],
 })
 export class AppModule {}
